2eb4279bdb565b1c14091d59a417eb392deca3e3
yuusheng <hi@yuusheng.im>
2024-08-02 18:14:12 +0800

    feat: basic grpc server

---
+++ src/client.rs
@@ -0,0 +1,22 @@
+use std::error::Error;
+
+use proto::calculator_client::CalculatorClient;
+
+pub mod proto {
+    tonic::include_proto!("calculator");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "http://[::1]:50051";
+    let mut client = CalculatorClient::connect(url).await?;
+
+    let req = proto::CalculationRequest { a: 4, b: 0 };
+    let request = tonic::Request::new(req);
+
+    let response = client.divide(request).await?;
+
+    println!("Response: {:?}", response.get_ref().result);
+
+    Ok(())
+}
